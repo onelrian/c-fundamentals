85dc4a0d0b84ed21b0f0e03096110202cb385ba6
onelrian <onelsob57@gmail.com>
2025-03-20 13:50:55 +0100

    Fix: Modified Ohm’s Law program with improved formatting and precision

---
+++ src/ohm's_law.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Declare variables
+    int R = 10;
+    float V = 3.3;
+    float I = V / R; // Calculate current using Ohm’s Law: I = V/R
+
+    // Print the result
+    printf("The value of current, I is %.2f A\n", I);
+
+    return 0;
+}
